47f058c75c2294ed4f84e08b41f057f1e6b25880
AlGouvea <alvarohsgouvea@gmail.com>
2021-09-01 19:52:15 -0300

    Update

--- main.c
+++ main.c
@@ -9,17 +9,7 @@
 
 //Time Inicio
 struct timeval inicio, overall_start, overall_end;
-int error, flag = 0;
-
-int format(double a) {
-    while(a >= 10) {
-        a /= 10;
-    }
-
-    int res = (int)a;
-    
-    return res;
-}
+int error = 0, flag = 0;
 
 int main() {
     //Recebe PATH
@@ -39,19 +29,20 @@ int main() {
         if (pid==0) {
             execl(PATH, PATH, arg, NULL);  
             if (strcmp(strerror(errno), "Success") != 0){
+                printf("Erro: %s\n", strerror(errno));
                 error = errno;
-                printf("Erro: %s\n", strerror(error));
-                exit(0);
+                exit(error);
             }
         }
-        if (wait(NULL)) {
+        if (wait(&error)) {
+            error = WEXITSTATUS(error);
             struct timeval final;
             gettimeofday(&final, NULL);
-            printf("Demorou %.1d.%.1d segundos, retornou %d\n", format(final.tv_sec - inicio.tv_sec), format(final.tv_usec - inicio.tv_usec), error);
+            printf("Demorou %.1lf segundos, retornou %d\n", (double)(final.tv_usec - inicio.tv_usec) / 1000000 + (double)(final.tv_sec - inicio.tv_sec), error);
         }
     }
     gettimeofday(&overall_end, NULL);
 
-    printf("O tempo total foi de %.1d.%.1d segundos\n", format(overall_end.tv_sec - overall_start.tv_sec), format(overall_end.tv_usec - overall_start.tv_usec));
+    printf("O tempo total foi de %.1lf segundos\n", (double)(overall_end.tv_usec - overall_start.tv_usec) / 1000000 + (double)(overall_end.tv_sec - overall_start.tv_sec));
     exit(0);
 }
