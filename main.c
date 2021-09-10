a1b11b05407e105bef35cbecbbd6f4177f181e36
luisgaboardi <luis.ggaboardi@gmail.com>
2021-09-09 21:56:06 -0300

    Chopp essa manga

--- main.c
+++ main.c
@@ -28,10 +28,12 @@ int main() {
         
         //Se for o filho
         if (pid==0) {
-            execl(PATH, PATH, arg, NULL);  
+            execl(PATH, PATH, arg, NULL);
             if (strcmp(strerror(errno), "Success") != 0){
                 printf("> Erro: %s\n", strerror(errno));
+                fflush(stdout);
                 error = errno;
+                fclose(stdin);
                 exit(errno);
             }
         }
@@ -40,10 +42,12 @@ int main() {
             struct timeval final;
             gettimeofday(&final, NULL);
             printf("> Demorou %.1lf segundos, retornou %d\n", (double)(final.tv_usec - inicio.tv_usec) / 1000000 + (double)(final.tv_sec - inicio.tv_sec), error);
+            fflush(stdout);
         }
     }
     gettimeofday(&overall_end, NULL);
 
     printf(">> O tempo total foi de %.1lf segundos\n", (double)(overall_end.tv_usec - overall_start.tv_usec) / 1000000 + (double)(overall_end.tv_sec - overall_start.tv_sec));
+    fflush(stdout);
     exit(0);
 }
